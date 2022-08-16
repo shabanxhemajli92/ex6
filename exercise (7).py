be947e31e51dff55d3cb7d8d69b054f7d52566c0
Shaban <shabanxhemajli@live.com>
2022-08-16 11:10:10 +0200

    first commit

---
+++ exercise (7).py
@@ -0,0 +1,10 @@
+# Task 1
+for num in range(0, 100):
+    if num % 15 is 0:
+        print("FizzBuzz")
+    elif num % 3 is 0:
+        print("Fizz")
+    elif num % 5 is 0:
+        print("Buzz")
+    else:
+        print(num)
