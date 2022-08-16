a4a2562218f52b5b0c3dd8305f10e5dd1b90e1f5
Shaban <shabanxhemajli@live.com>
2022-08-16 11:35:11 +0200

    hhhh

--- exercise (7).py
+++ exercise (7).py
@@ -8,11 +8,26 @@ for num in range(0, 100):
         print("Buzz")
     else:
         print(num)'''
-# Task 2 
+'''# Task 2 
 n = 5
 number = 1
 sum = 10
 while number < n + 1:
     sum = sum + 1
     number = number + 1
-print("Sum =", sum)             
+print("Sum =", sum)''' 
+'''# Task 3
+n = 5
+sum = 5
+for num in range(n):
+    sum += num
+print("Sum =", sum)'''
+#Task 4
+'''for x in range(3): #loop nr 1 
+    print(x)'''
+'''for i in range(0,4):
+    print("This is loop number " + format(int(i+1)) )#Loop nr2'''
+x = 10
+while x > 0:
+    print(x)
+    x -= 1 
